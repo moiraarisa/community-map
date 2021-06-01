69714e7d3c76081699f2edc38e186391588ad519
moiraarisa <85144948+moiraarisa@users.noreply.github.com>
2021-05-31 22:38:57 -0700

    map setup good!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MapView from './Map';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+     
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload, Zeze!
@@ -15,8 +17,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        <MapView></MapView>
       </header>
     </div>
   );
